e5e3f7b2148f2041244ac846f0fd58f6abf78308
anastasia074 <129087701+anastasia074@users.noreply.github.com>
2023-06-15 13:52:36 +0400

    Add files via upload

--- dz.4/src/Main.java
+++ dz.4/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
         MyTreeSet<Integer> test = new MyTreeSet<>();
+        TreeSet testSet;
+        testSet.add(12);
+        testSet.remove(12);
+        testSet.clear();
     }
 }
