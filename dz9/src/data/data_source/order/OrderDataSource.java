20bbbdea38b09295a0774f734eaabf2c7faf7b41
anastasia074 <129087701+anastasia074@users.noreply.github.com>
2023-07-03 16:10:18 +0400

    Delete dz9 directory

--- dz9/src/data/data_source/order/OrderDataSource.java
+++
@@ -1,9 +0,0 @@
-package data.data_source.order;
-
-import data.models.Order;
-
-public abstract class OrderDataSource {
-    //источник данных
-    public abstract Order getOrder();
-    public abstract void createOrder(Order order);
-}
