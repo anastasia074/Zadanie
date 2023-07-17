e1cf2435712b97cbca648ba39a89956ce1a97001
anastasia074 <129087701+anastasia074@users.noreply.github.com>
2023-07-17 12:23:03 +0400

    Add files via upload

---
+++ Ds.4/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        MyTreeSet<Integer> test = new MyTreeSet<>();
+        test.add(12);
+        test.remove(12);
+        test.contains(12);
+    }
+}
